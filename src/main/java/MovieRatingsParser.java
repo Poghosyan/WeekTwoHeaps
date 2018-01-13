de6263a95899d194a5be082dc38f1380cd3c2737
Mher <poghosyanusc@gmail.com>
2018-01-13 15:45:14 -0800

    Added instructions and documentation for methods to implement

--- src/main/java/MovieRatingsParser.java
+++ src/main/java/MovieRatingsParser.java
@@ -10,6 +10,34 @@ import java.util.TreeMap;
 
 public class MovieRatingsParser {
 
+	/**
+	 * Takes a List of UserMovieRatings as input and creates a TreeMap data structure from it. The TreeMap is a mapping
+     * from the movie’s title to a PriorityQueue of all its associated ratings.
+     *
+     * Keep in mind that the same movie title may appear numerous times in the input List, and multiple instances may
+     * even have the same rating.
+     *
+     * A distinct movie title should only appear once as a key in the TreeMap, however, and be mapped to a PriorityQueue
+     * (min-heap) of all the ratings for that movie.
+     *
+     * Your implementation of parseMovieRatings should also adhere to the following:
+     *
+     * If the input List is null or empty, parseMovieRatings should return an empty TreeMap
+     * If the input List contains any null UserMovieRatings object, or a UserMovieRatings object whose movie title is
+     * null or an empty string, or a UserMovieRatings object whose rating is negative, parseMovieRatings should ignore
+     * that UserMovieRatings object
+     * The movie titles should be considered case-insensitive, i.e. if two UserMovieRatings objects have the same title
+     * that differ only in case (upper or lower), they should be considered the same movie. The movie titles stored in
+     * the TreeMap must use lowercase letters.
+     *
+     * Please do not change the signature of the parseMovieRatings method and please do not modify UserMovieRatings.java.
+     * Also, please do not create any additional .java files for your solution. If you need additional classes, you can
+     * define them in MovieRatingsParser.java. Last, please be sure that your MovieRatingsParser class is in the default
+     * package, i.e. there is no “package” declaration at the top of the source code.
+     *
+	 * @param allUsersRatings
+	 * @return
+	 */
 	public static TreeMap<String, PriorityQueue<Integer>> parseMovieRatings(List<UserMovieRating> allUsersRatings) {
 		
 		/* IMPLEMENT THIS METHOD! */
--- src/main/java/MovieRatingsProcessor.java
+++ src/main/java/MovieRatingsProcessor.java
@@ -11,6 +11,15 @@ import java.util.TreeMap;
 
 public class MovieRatingsProcessor {
 
+    /**
+     * return a List of movie titles in alphabetical order
+     *
+     * If the input is null or an empty TreeMap, the data structure returned by the method should be empty
+     * You can assume that all movie titles in the input TreeMap consist of lowercase letters.
+     *
+     * @param movieRatings
+     * @return
+     */
 	public static List<String> getAlphabeticalMovies(TreeMap<String, PriorityQueue<Integer>> movieRatings) {
 		
 		/* IMPLEMENT THIS METHOD! */
@@ -18,13 +27,40 @@ public class MovieRatingsProcessor {
 		return null; // this line is here only so this code will compile if you don't modify it
 	}
 
+    /**
+     * given an input int rating, return a List of movie titles in alphabetical order, where all movies in the List do
+     * not have any ratings less than or equal to rating (hint: the PriorityQueue is a min-heap, meaning that the
+     * smallest rating is at the front of the queue!)
+     *
+     * If the input is null or an empty TreeMap, the data structure returned by the method should be empty
+     * You can assume that all movie titles in the input TreeMap consist of lowercase letters.
+     *
+     * @param movieRatings
+     * @param rating
+     * @return
+     */
 	public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
 		
 		/* IMPLEMENT THIS METHOD! */
 		
 		return null; // this line is here only so this code will compile if you don't modify it
 	}
-	
+
+    /**
+     * given an input int rating, modify the TreeMap object that was passed as an argument so that you remove all
+     * ratings in the PriorityQueue that are below (but not equal to) rating for every movie in the Map. If all ratings
+     * are removed from a movie’s PriorityQueue, then remove the mapping from the TreeMap. Additionally, this method
+     * should return a new TreeMap that maps a movie title to the number of ratings that were removed from its
+     * corresponding PriorityQueue; the TreeMap that is returned should only contain movies that had ratings removed
+     * from its PriorityQueue.
+     *
+     * If the input is null or an empty TreeMap, the data structure returned by the method should be empty
+     * You can assume that all movie titles in the input TreeMap consist of lowercase letters.
+     *
+     * @param movieRatings
+     * @param rating
+     * @return
+     */
 	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
 		
 		/* IMPLEMENT THIS METHOD! */
