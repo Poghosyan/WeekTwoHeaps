cbf7d4237bf13aa72929793ca9e76e766e422abc
Mher <poghosyanusc@gmail.com>
2018-01-16 23:16:48 -0800

    Implemented removeAllRatingsBelow and passed all tests. The assignment passed all the Homework5Grader tests.

--- src/main/java/MovieRatingsProcessor.java
+++ src/main/java/MovieRatingsProcessor.java
@@ -4,6 +4,8 @@
  * Please be sure not to change the method signatures!
  */
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.*;
 
 
@@ -15,13 +17,13 @@ public class MovieRatingsProcessor {
      * @param movieRatings
      * @return
      */
-	public static List<String> getAlphabeticalMovies(TreeMap<String, PriorityQueue<Integer>> movieRatings) {
-		if (movieRatings == null || movieRatings.isEmpty()) {
-			return new ArrayList<String>();
-		}
+    public static List<String> getAlphabeticalMovies(TreeMap<String, PriorityQueue<Integer>> movieRatings) {
+        if (movieRatings == null || movieRatings.isEmpty()) {
+            return new ArrayList<String>();
+        }
 
-		return new ArrayList<String>(movieRatings.navigableKeySet());
-	}
+        return new ArrayList<String>(movieRatings.navigableKeySet());
+    }
 
     /**
      * given an input int rating, return a List of movie titles in alphabetical order, where all movies in the List do
@@ -32,18 +34,19 @@ public class MovieRatingsProcessor {
      * @param rating
      * @return
      */
-	public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
-		if (movieRatings == null || movieRatings.isEmpty()) {
-			return new ArrayList<String>();
-		}
+    public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
+        if (movieRatings == null || movieRatings.isEmpty()) {
+            return new ArrayList<String>();
+        }
 
-		ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> result = new ArrayList<String>();
 
-		for (Map.Entry<String, PriorityQueue<Integer>> entry : movieRatings.entrySet()) {
-		    PriorityQueue<Integer> ratings =  entry.getValue();
+        for (Map.Entry<String, PriorityQueue<Integer>> entry : movieRatings.entrySet()) {
+            PriorityQueue<Integer> ratings = entry.getValue();
 
-		    if (rating >= ratings.peek()) {
-		        continue;
+            //TODO: What needs to happen when no ratings are in the PriorityQueue?
+            if (rating >= ratings.peek()) {
+                continue;
             }
 
             //TODO: could possibly use Binary Search Tree to get alphabetical order for practice
@@ -52,28 +55,51 @@ public class MovieRatingsProcessor {
         }
 
         Collections.sort(result);
-		return result;
-	}
+        return result;
+    }
 
     /**
      * given an input int rating, modify the TreeMap object that was passed as an argument so that you remove all
-     * ratings in the PriorityQueue that are below (but not equal to) rating for every movie in the Map. If all ratings
-     * are removed from a movie’s PriorityQueue, then remove the mapping from the TreeMap. Additionally, this method
-     * should return a new TreeMap that maps a movie title to the number of ratings that were removed from its
-     * corresponding PriorityQueue; the TreeMap that is returned should only contain movies that had ratings removed
-     * from its PriorityQueue.
-     *
-     * If the input is null or an empty TreeMap, the data structure returned by the method should be empty
-     * You can assume that all movie titles in the input TreeMap consist of lowercase letters.
+     * ratings in the PriorityQueue that are below rating for every movie in the Map.
+     * <p>
+     * If all rating are removed from a movie’s PriorityQueue, then remove the mapping from the TreeMap.
+     * <p>
+     * Additionally, this method should return a new TreeMap that maps a movie title to the number of ratings that were
+     * removed from its corresponding PriorityQueue; the TreeMap that is returned should only contain movies that had
+     * ratings removed from its PriorityQueue.
      *
      * @param movieRatings
      * @param rating
      * @return
      */
-	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
-		
+    public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating) {
+        //TODO: Could this be optimized somehow? What's another method for modifying moveRatings
+        if (movieRatings == null || movieRatings.isEmpty()) {
+            return new TreeMap<String, Integer>();
+        }
 
-		
-		return null;
-	}
+        TreeMap<String, PriorityQueue<Integer>> tempRatings = new TreeMap<String, PriorityQueue<Integer>>(movieRatings);
+        TreeMap<String, Integer> result = new TreeMap<String, Integer>();
+
+        for (Map.Entry<String, PriorityQueue<Integer>> entry : tempRatings.entrySet()) {
+            PriorityQueue<Integer> ratings = entry.getValue();
+            Integer counter = 0;
+
+            while (!ratings.isEmpty() && ratings.peek() < rating) {
+                ratings.remove();
+                counter++;
+            }
+
+            if (counter > 0) {
+                result.put(entry.getKey(), counter);
+                movieRatings.put(entry.getKey(), ratings);
+            }
+
+            if (ratings.isEmpty()) {
+                movieRatings.remove(entry.getKey());
+            }
+        }
+
+        return result;
+    }
 }
